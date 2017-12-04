ea3e625ac75a255356b379a171bfd8aefc779365
Lucas Tambarin <l.tambarin@gmail.com>
2017-12-04 18:17:59 +0100

    Modify view

--- polls/models.py
+++ polls/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Todo(models.Model):
     todo_text = models.CharField(max_length=200)
     expired_date = models.DateTimeField('date expire')
+
+    def __str__(self):
+        return "You have to "+self.todo_text+" before "+self.expired_date
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Todo
+
 def index(request):
-    return HttpResponse("Welcome on the index !")
+    todo_list = Todo.objects.order_by('-expired_date')[:5]
+    output = ','.join([t.todo_text for t in todo_list])
+    return HttpResponse(output)
+    
 
 # Create your views here.
