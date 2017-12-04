2196f6bcf5fecb496482e03eac5c74165fa28998
Lucas Tambarin <l.tambarin@gmail.com>
2017-12-04 17:58:34 +0100

    Add first model

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    todo_text = models.CharField(max_length=200)
+    expired_date = models.DateTimeField('date expire')
