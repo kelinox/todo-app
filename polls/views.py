043be92c8b049b2e7a843602235d3f07f7631a62
Lucas Tambarin <l.tambarin@gmail.com>
2017-12-04 18:24:14 +0100

    Add template

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Todo
 
 def index(request):
     todo_list = Todo.objects.order_by('-expired_date')[:5]
-    output = ','.join([t.todo_text for t in todo_list])
-    return HttpResponse(output)
+    context = {
+        'todo_list': todo_list,
+    }
+    return render(request,'polls/index.html',context)
     
 
 # Create your views here.
