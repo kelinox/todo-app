ee367c80598c1eedc073658b3d37b784abaabdce
Lucas Tambarin <l.tambarin@gmail.com>
2017-12-05 21:43:16 +0100

    Can add todo to db and check date

--- polls/views.py
+++ polls/views.py
@@ -1,32 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.template import loader
+from datetime import date
 
 from .models import Todo
 from .forms import TodoForm
 
 def index(request):
     todo_list = Todo.objects.order_by('-expired_date')[:5]
+    error = ""
     if request.method == 'POST':
         form = TodoForm(request.POST)
         if form.is_valid():
-            todo_name = form.cleaned_data['todo_name']
             expired_date = form.cleaned_data['expired_date']
-            todo = Todo(todo_text=todo_name,expired_date=expired_date)
-            todo.save()
+            date_today = date.today()
+            if expired_date > date_today:
+                todo_name = form.cleaned_data['todo_name']
+                todo = Todo(todo_text=todo_name,expired_date=expired_date)
+                todo.save()
+            else:
+                error = "You can't add a todo with this date"
         else:
-            todo_name = "Not clean"
-            expired_date = "Not clean"
+            error="Form not clean"
     else:
         form = TodoForm()
-        todo_name = "null"
-        expired_date = "Null"
 
     context = {
         'todo_list': todo_list,
         'form': form,
-        'todo_name': todo_name,
-        'expired_date': expired_date,
+        'error': error,
     }
     return render(request,'polls/index.html',context)
     
