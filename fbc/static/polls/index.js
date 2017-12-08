cb90a4c2bc131c0550c6c34ec36fd11d86dc5ae9
Lucas Tambarin <l.tambarin@gmail.com>
2017-12-08 18:59:15 +0100

    Change css, add sortable

--- fbc/static/polls/index.js
+++ fbc/static/polls/index.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
+    $('#todo_list').sortable();
     $('#form').submit(function(e){
-        e.preventDefault();
+        e.preventDefault({
+            'placeholder':true,
+            'zindex': 1
+        });
         $.post('/', $(this).serialize(), function(data){ 
             data = JSON.parse(data);
-            $('#todo_list').append('<li id="'+ data.todo_id+'">'+ data.todo_name+'</li>')
-            console.log(data.todo);
+            $('#todo_list').append('<li id="'+ data.todo_id+'">'+ data.todo_name+'</li>');
         });
     });
 })
--- polls/forms.py
+++ polls/forms.py
@@ -2,5 +2,7 @@ from django import forms
 import datetime
 
 class TodoForm(forms.Form):
-    todo_name = forms.CharField(label='Todo : ',max_length=100)
+    todo_name = forms.CharField(label='',max_length=100,
+                                widget=forms.TextInput(attrs={'placeholder': 'What\' to be done ?'})
+    )
     expired_date = forms.DateField(initial=datetime.date.today,required=False)
